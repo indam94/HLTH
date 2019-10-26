4590c8a7e5d96da49abf7f36b225e22159d1f126
DONGGUN LEE <d0n99un17@gmail.com>
2019-10-26 16:24:14 -0700

    UserName Show

--- IF_Temp/ViewControllers/MainViewController.swift
+++ IF_Temp/ViewControllers/MainViewController.swift
@@ -7,15 +7,52 @@
 //
 
 import UIKit
+import Charts
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var pieChart: PieChartView!
+    @IBOutlet weak var helloUser: UILabel!
+    
+    //Temp Chart Data
+    var tempData1 = PieChartDataEntry(value: 0)
+    var tempData2 = PieChartDataEntry(value: 0)
+    
+    var numberOfDownloadDataEntries = [PieChartDataEntry]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let username: String? = UserPersonalProfile.getUserName()
+        helloUser.text = "Hello, \(username ?? "Unknown")!"
 
         // Do any additional setup after loading the view.
+        
+        pieChart.chartDescription?.text? = "Main Chart Description"
+        
+        tempData1.value = 50
+        tempData1.label = "temp1"
+        
+        tempData2.value = 50
+        tempData2.label = "temp2"
+        
+        numberOfDownloadDataEntries = [tempData1, tempData2]
+        
+        updateMainChartData()
     }
     
+    func updateMainChartData(){
+        
+        let chartDataSet = PieChartDataSet(entries: numberOfDownloadDataEntries, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor(named: "main"), UIColor(named: "secondary")]
+        
+        chartDataSet.colors = colors as! [NSUIColor]
+        
+        pieChart.data = chartData
+        
+    }
 
     /*
     // MARK: - Navigation
