44443f1c5e17afefce8a80d45e257f6c7e570411
DONGGUN LEE <d0n99un17@gmail.com>
2019-10-27 02:51:37 -0700

    Temp Commit

--- IF_Temp/ViewControllers/LoginViewController.swift
+++ IF_Temp/ViewControllers/LoginViewController.swift
@@ -24,12 +24,55 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         submitButton.isEnabled = false
+        
+        self.view.layer.backgroundColor = UIColor.green.cgColor
+        
+        //setLayout()
+    }
+    
+    func setLayout(){
+        let label = UILabel()
+        label.frame = CGRect(x: 0, y: 0, width: 335, height: 48)
+        label.backgroundColor = .white
+        label.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+        self.view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+        label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 431).isActive = true
+        
+        let label1 = UILabel()
+        label1.frame = CGRect(x: 0, y: 0, width: 335, height: 48)
+        label1.backgroundColor = .white
+        label1.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+        self.view.addSubview(label1)
+        
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        label1.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        label1.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+        label1.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 333).isActive = true
+        
+        let label2 = UILabel()
+        label2.frame = CGRect(x: 0, y: 0, width: 335, height: 48)
+        label2.backgroundColor = .white
+        label2.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
+        self.view.addSubview(label2)
+        
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        label2.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        label2.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 17).isActive = true
+        label2.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 529).isActive = true
+        
     }
     
     @IBAction func onSubmit(_ sender: Any) {
         
         if let username = usernameTextField.text {
-            UserPersonalProfile.setUserName(name: username)
+            //UserPersonalProfile.setUserName(name: username)
             
             //go to main
             let mainVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainViewController")
